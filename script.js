c8a9dfd95ad8bfcfdb61d7db4d1bc2ef6d5f060a
Jackeline Lopes <100817655+jackjoysslopes@users.noreply.github.com>
2022-03-23 23:00:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+function Linkedin() {
+  var win = window.open(
+    "https://www.linkedin.com/in/jackeline-de-souza-santos-lopes-2a79b91ab"
+  );
+}
+function Github() {
+  var win = window.open("https://github.com/jackjoysslopes");
+}
